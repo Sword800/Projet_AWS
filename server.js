48f980f36247786b7f7effe1f95d171f695601c0
Glitch (aws-projet-joran-maxime) <none>
2019-04-27 13:22:39 +0000

    🌧🏁 Updated with Glitch

--- server.js
+++ server.js
@@ -25,9 +25,12 @@ app.use(session({
 
 function checkHttps(req, res, next){
   // protocol check, if http, redirect to https
-  if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
-    return next()
-  } else {
+  if(req.get('X-Forwarded-Proto').indexOf("https")!=-1)
+  {
+    return next();
+  } 
+  else 
+  {
     res.redirect('https://' + req.hostname + req.url);
   }
 }
--- sockets.js
+++ sockets.js
@@ -1,48 +1,49 @@
 module.exports = function(io, knex) {
   let connected_users = {}
   
-  function getKeyByPseudo(object, value) {
+  function getKeyByPseudo(object, value) 
+  {
     return Object.keys(object).find(key => object[key].pseudo === value);
   }
   
-  function zeros(dimensions) {
+  function zeros(dimensions) 
+  {
     var array = [];
 
-    for (var i = 0; i < dimensions[0]; ++i) {
+    for (var i = 0; i < dimensions[0]; ++i) 
+    {
         array.push(dimensions.length == 1 ? 0 : zeros(dimensions.slice(1)));
     }
 
     return array;
-}
-  
-  function startGame(id1, id2){
-    let board = {
-      p1: id1,
-      p2: id2,
-      arr: zeros([120, 120])
-    }
-    board.arr[120, 60] = 1
-    board.arr[0, 60] = 2
   }
   
-  function game(board, id1, id2){
-    let movep1 = connected_users[id1].move
-    let movep2 = connected_users[id2].move
-    
+  function startGame(id1, id2)
+  {
+    let board = {p1: id1,p2: id2,arr: zeros([120, 120])};
+    board.arr[120, 60] = 1;
+    board.arr[0, 60] = 2;
   }
   
-  function createGame(player1, player2){
+  function game(board, id1, id2)
+  {
+    let movep1 = connected_users[id1].move;
+    let movep2 = connected_users[id2].move;
+  }
+  
+  function createGame(player1, player2)
+  {
     let idPlayer1 = getKeyByPseudo(connected_users, player1);
     let idPlayer2 = getKeyByPseudo(connected_users, player2);
     
-    console.log(connected_users)
+    console.log(connected_users);
     io.to(idPlayer1).emit('createGame');
     io.to(idPlayer2).emit('createGame');
     
-    startGame(idPlayer1, idPlayer2)
+    startGame(idPlayer1, idPlayer2);
    }
   
-  io.on('connection', function(socket){
+   io.on('connection', function(socket){
     console.log('a user connected');
     let handshakeData = socket.request;
     let pseudo = handshakeData._query['pseudo'];
@@ -57,7 +58,7 @@ module.exports = function(io, knex) {
     let lose
 
 
-    knex('user').select()
+    knex('users').select()
                 .where('pseudo', pseudo)
                 .then((rows) => {
       win = rows[0].win
